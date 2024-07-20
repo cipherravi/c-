0dede64386cdaff7eece29033ecf12e31c7d6091
cipherravi <ravi4personal9431@gmail.com>
2024-07-20 11:52:21 +0530

    Pattern end

--- Practice/Practice_july/hello_world.cpp
+++ Practice/Practice_july/hello_world.cpp
@@ -4,5 +4,8 @@ int main()
 {
  
 cout<<"hello world"<<endl;
-
+ unsigned long a = 3434343343434343433;
+ long  b = 3343434343433333333;
+ cout <<a<<endl;
+ cout <<b<<endl;
 }
--- Practice/Practice_july/patterns.cpp
+++ Practice/Practice_july/patterns.cpp
@@ -145,7 +145,7 @@ int main()
   //   cout << endl;
   // }
 
-  //Full Inverted Pyramid
+  // Full Inverted Pyramid
 
   //  int n;
   // cin >> n;
@@ -163,39 +163,38 @@ int main()
 
   //   cout << endl;
   // }
-  //if we use both full pyraid at same time it will create a diamond
-
+  // if we use both full pyraid at same time it will create a diamond
 
   // Full Hollow Pyramid
-/*
-  int n;
-  cin >> n;
+  /*
+    int n;
+    cin >> n;
 
-  for (int row = 1; row <= n; row++)
-  {
-    for (int space = 1; space <= n - row; space++)
+    for (int row = 1; row <= n; row++)
     {
-      cout << " ";
+      for (int space = 1; space <= n - row; space++)
+      {
+        cout << " ";
+      }
+      for (int col = 1; col <= 1; col++)
+      {
+        cout << "*";
+      }
+      if (row>=2)
+      {
+      for (int space = 1; space <= (row*2)-3; space++)
+      {
+        cout << " ";
+      }
+      for (int col = 1; col <= 1; col++)
+      {
+        cout << "*";
+      }
+      }
+      cout << endl;
     }
-    for (int col = 1; col <= 1; col++)
-    {
-      cout << "*";
-    }
-    if (row>=2)
-    {
-    for (int space = 1; space <= (row*2)-3; space++)
-    {
-      cout << " ";
-    }
-    for (int col = 1; col <= 1; col++)
-    {
-      cout << "*";
-    }
-    }
-    cout << endl;
-  }
-*/
-  //Full Inverted Hollow Pyramid
+  */
+  // Full Inverted Hollow Pyramid
 
   //  int n;
   // cin >> n;
@@ -224,34 +223,189 @@ int main()
     cout << endl;
   }
   */
-  //if we use both full hollow pyraid at same time it will create a hollow diamond
+  // if we use both full hollow pyraid at same time it will create a hollow diamond
 
-// Flipped solid Diamond 
+  // Flipped solid Diamond
+  /*
+  int n;
+  cin>>n;
 
-int n;
-cin>>n;
-
-for (int i = n; i >= 1; i--)
-{
-  for (int j = 1; j <= i; j++)
+  for (int i = 0; i < n; i++)
   {
-    cout<<"*";
-   
-  }
-   for (int k = 1; k <= (i*2)-3; k++)
-  {
-    cout<<" ";
-    
-  }
-  for (int l = 1; l <=i; l++)
+    for (int j = 0; j <n-i; j++)
     {
-    cout<<"*";
+      cout<<"*";
 
     }
-  cout<<endl;
-}
+     for (int k = 0; k < 2*i+1; k++)
+    {
+      cout<<" ";
 
+    }
+    for (int j = 0; j <n-i; j++)
+    {
+      cout<<"*";
 
+    }
 
+    cout<<endl;
+  }
 
+  for (int i = n; i > 0; i--)
+  {
+    for (int j = 0; j <=n-i; j++)
+    {
+      cout<<"*";
+
+    }
+     for (int k = 0; k < 2*i-1; k++)
+    {
+      cout<<" ";
+
+    }
+    for (int j = 0; j <=n-i; j++)
+    {
+      cout<<"*";
+
+    }
+
+    cout<<endl;
+  }
+  */
+
+  // Fancy pattern
+  /*
+    int n;
+    cin>>n;
+
+    for (int row = 0; row < n; row++)
+    {
+      for (int col = 0; col <= row; col++)
+      {
+        cout << row + 1;
+        if (col != row)
+        {
+          cout << "*";
+        }
+      }
+      cout << endl;
+    }
+    for (int row = 0; row < n; row++)
+    {
+      for (int col = 0; col < n-row; col++)
+      {
+        cout << n-row;
+        if (col != n-row-1)
+        {
+          cout << "*";
+        }
+      }
+      cout << endl;
+    }
+    */
+
+  // Alphabetical palindrome pyramid
+  /*
+    int n;
+    cin >> n;
+
+    for (int row = 0; row < n; row++)
+    {
+      for (int col = 0; col <= row; col++)
+      {
+        int ans = col + 1;
+        char ch = ans + 'A' - 1;
+        cout << ch;
+        // cout<<col+1;
+      }
+      for (int col = row; col > 0; col--)
+      {
+        int ans = col ;
+        char ch = ans + 'A' - 1;
+        cout << ch;
+        // cout<<col;
+      }
+
+      cout << endl;
+    }
+    */
+  /*
+    ////  Numeric Full Pyramid
+    int n;
+    cin>>n;
+    for (int row = 0; row < n; row++)
+    {
+      //spaces
+      for (int space = 0; space < n-row-1; space++)
+      {
+        cout<<" ";
+      }
+      //left number triangle
+      // int start=row+1;
+      for (int col = 0; col < row+1; col++)
+      {
+        cout<< row+col+1<<"";
+        // cout<< start<<"";
+        // start++;/
+
+      }
+      //right reverse number trianle
+      for (int right = 2*row; right > row; right--)
+      {
+        cout<<right;
+      }
+
+      cout<<endl;
+    }
+    */
+
+  ////Numeric Hollw Pyramid
+  /*
+    int n;
+    cin >> n;
+
+    for (int row = 0; row < n; row++)
+    {
+      // spaces
+      for (int space = 0; space < n - row - 1; space++)
+      {
+        cout << " ";
+      }
+      if (row == n - 1)
+      {
+        // last row
+        for (int last = 1; last < n; last++)
+        {
+          cout << last << " ";
+        }
+      }
+
+      if (row != n - 1)
+      {
+        // left 1
+        for (int col = 1; col <= 1; col++)
+        {
+          cout << col;
+        }
+        // mid spaces
+        for (int midspace = 0; midspace < row * 2 - 1; midspace++)
+        {
+          cout << " ";
+        }
+      }
+
+      if (row >= 1)
+      {
+        // right counting
+        for (int right = row + 1; right <= row + 1; right++)
+        {
+          cout << right;
+        }
+      }
+
+      cout << endl;
+    }
+    */
+
+  ////End
 }
